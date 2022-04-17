d28f07be6d9bccdf220e62adcec9f4d3569681f8
donquixxy <agusary726@gmail.com>
2022-04-17 22:22:26 +0800

    update

--- lib/app/modules/addData/views/add_data_view.dart
+++ lib/app/modules/addData/views/add_data_view.dart
@@ -29,129 +29,135 @@ class AddDataView extends GetView<AddDataController> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Obx(
-            () => SizedBox(
-              height: MediaQuery.of(context).size.height,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  imagePickerController.adaIsi.isTrue
-                      ? Image.file(
-                          imagePickerController.image!,
-                          height: 150,
-                          width: 300,
-                          fit: BoxFit.fill,
-                        )
-                      : GestureDetector(
-                          onTap: () {
-                            imagePickerController.pickImageFrom();
-                          },
-                          child: Container(
-                            alignment: Alignment.center,
-                            margin: const EdgeInsets.all(15),
-                            height: 150,
-                            width: 300,
-                            child: const Text(
-                              "Insert Image",
-                              textAlign: TextAlign.center,
-                            ),
-                            decoration: BoxDecoration(border: Border.all()),
-                          ),
-                        ),
-                  Container(
-                    margin: const EdgeInsets.all(12),
-                    child: TextFormField(
-                      controller: controller.namaResepController,
-                      keyboardType: TextInputType.multiline,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 25, horizontal: 20),
-                          label: const Text("Nama Resep")),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(12),
-                    child: TextFormField(
-                      controller: controller.cookTimeController,
-                      keyboardType: TextInputType.multiline,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 25, horizontal: 20),
-                          label: const Text("Durasi Masak")),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(12),
-                    child: TextFormField(
-                      controller: controller.deskripsiController,
-                      minLines: 1,
-                      maxLines: null,
-                      keyboardType: TextInputType.multiline,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 25, horizontal: 20),
-                          label: const Text("Deskripsi")),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
+            () => controller.isLoading.value
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        imagePickerController.adaIsi.isTrue
+                            ? Image.file(
+                                imagePickerController.image!,
+                                height: 150,
+                                width: 300,
+                                fit: BoxFit.fill,
+                              )
+                            : GestureDetector(
+                                onTap: () {
+                                  imagePickerController.pickImageFrom();
+                                },
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  margin: const EdgeInsets.all(15),
+                                  height: 150,
+                                  width: 300,
+                                  child: const Text(
+                                    "Insert Image",
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  decoration:
+                                      BoxDecoration(border: Border.all()),
+                                ),
+                              ),
+                        Container(
                           margin: const EdgeInsets.all(12),
                           child: TextFormField(
-                            controller: controller.chipWordController,
-                            maxLines: null,
-                            onFieldSubmitted: controller.addDataToChip,
-                            textInputAction: TextInputAction.done,
+                            controller: controller.namaResepController,
+                            keyboardType: TextInputType.multiline,
                             decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20)),
-                              contentPadding: const EdgeInsets.symmetric(
-                                  vertical: 25, horizontal: 20),
-                              label: const Text("Bahan Baku"),
-                            ),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 25, horizontal: 20),
+                                label: const Text("Nama Resep")),
                           ),
                         ),
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            controller.addDataToChip(
-                                controller.chipWordController.text);
-                          },
-                          child: const Text("Add"))
-                    ],
-                  ),
-                  Obx((() => Expanded(
-                        child: GridView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 4,
-                                  crossAxisSpacing: 2,
-                                  mainAxisSpacing: 2),
-                          itemCount: controller.chipWord.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Chip(
-                              onDeleted: () {
-                                controller.deleteDataChip(index);
-                              },
-                              backgroundColor: Colors.primaries[
-                                  Random().nextInt(Colors.primaries.length)],
-                              labelPadding: const EdgeInsets.all(4),
-                              label: Text(controller.chipWord[index]),
-                            );
-                          },
+                        Container(
+                          margin: const EdgeInsets.all(12),
+                          child: TextFormField(
+                            controller: controller.cookTimeController,
+                            keyboardType: TextInputType.multiline,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 25, horizontal: 20),
+                                label: const Text("Durasi Masak")),
+                          ),
                         ),
-                      )))
-                ],
-              ),
-            ),
+                        Container(
+                          margin: const EdgeInsets.all(12),
+                          child: TextFormField(
+                            controller: controller.deskripsiController,
+                            minLines: 1,
+                            maxLines: null,
+                            keyboardType: TextInputType.multiline,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 25, horizontal: 20),
+                                label: const Text("Deskripsi")),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                margin: const EdgeInsets.all(12),
+                                child: TextFormField(
+                                  controller: controller.chipWordController,
+                                  maxLines: null,
+                                  onFieldSubmitted: controller.addDataToChip,
+                                  textInputAction: TextInputAction.done,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    contentPadding: const EdgeInsets.symmetric(
+                                        vertical: 25, horizontal: 20),
+                                    label: const Text("Bahan Baku"),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            TextButton(
+                                onPressed: () {
+                                  controller.addDataToChip(
+                                      controller.chipWordController.text);
+                                },
+                                child: const Text("Add"))
+                          ],
+                        ),
+                        Obx((() => Expanded(
+                              child: GridView.builder(
+                                physics: const NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                gridDelegate:
+                                    const SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 4,
+                                        crossAxisSpacing: 2,
+                                        mainAxisSpacing: 2),
+                                itemCount: controller.chipWord.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Chip(
+                                    onDeleted: () {
+                                      controller.deleteDataChip(index);
+                                    },
+                                    backgroundColor: Colors.primaries[Random()
+                                        .nextInt(Colors.primaries.length)],
+                                    labelPadding: const EdgeInsets.all(4),
+                                    label: Text(controller.chipWord[index]),
+                                  );
+                                },
+                              ),
+                            )))
+                      ],
+                    ),
+                  ),
           ),
         ),
       ),
